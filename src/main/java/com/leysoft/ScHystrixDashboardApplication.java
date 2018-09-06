d24ad1473c1651d15deb281c5ad5fe0f1abd4a4a
Will Leyton <will.leyton@rappi.com>
2018-09-06 13:44:27 -0500

    Updated

--- src/main/java/com/leysoft/ScHystrixDashboardApplication.java
+++ src/main/java/com/leysoft/ScHystrixDashboardApplication.java
@@ -1,3 +1,4 @@
+
 package com.leysoft;
 
 import org.springframework.boot.SpringApplication;
@@ -9,7 +10,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
 @SpringBootApplication
 public class ScHystrixDashboardApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScHystrixDashboardApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ScHystrixDashboardApplication.class, args);
+    }
 }
